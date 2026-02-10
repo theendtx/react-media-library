d2f99ef19a0926945cae4b9400ea12c549aab3db
Kanatbek <kanatbekabad@gmail.com>
2026-02-10 22:24:51 +0500

    feat: add semantic layout and base UI structure

---
+++ media-library/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+    return (
+        <footer>
+            <small>© 2026</small>
+        </footer>
+    )
+}
+
+export default Footer;
---
+++ media-library/src/components/Main.jsx
@@ -0,0 +1,15 @@
+function Main() {
+  return (
+    <main className="main page container">
+      <section className="mainSection addSection">
+        <h2>Add new item</h2>
+      </section>
+
+      <section className="mainSection listSection">
+        <h2>My items</h2>
+      </section>
+    </main>
+  )
+}
+
+export default Main
